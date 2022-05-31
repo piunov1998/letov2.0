0ec8cc974cb2daf5a084f5c870660c6ce450d9de
Anton <piunov.doc@gmail.com>
2022-06-01 00:47:43 +0300

    [add]: stop, list, embed

--- src/cogs/music.py
+++ src/cogs/music.py
@@ -1,4 +1,5 @@
 import os.path
+from asyncio import run_coroutine_threadsafe, CancelledError
 
 import discord
 import sqlalchemy as sa
@@ -21,19 +22,46 @@ class Music(commands.Cog):
     def format_message(cls, msg, **_):
         return f'```{msg}```'
 
+    @classmethod
+    async def send_embed(cls, ctx: commands.Context, msg: str, *,
+                         title: str = None,
+                         color: discord.Colour = discord.Colour.blue(),
+                         url: str = None):
+        embed = discord.Embed()
+        if title:
+            embed.title = title
+        embed.description = msg
+        embed.colour = color
+        if url:
+            embed.url = url
+        await ctx.send(embed=embed)
+
     def get_song(self, ident: str | int) -> Song:
-        return self.pg.execute(sa.select(Song).where(Song.id == ident)).scalar_one_or_none()
+        return self.pg.execute(
+            sa.select(Song).where(Song.id == ident)).scalar_one_or_none()
 
-    async def connect(self):
-        pass
-
-    async def disconnect(self):
-        pass
+    @classmethod
+    async def disconnect(cls, ctx: commands.Context):
+        await ctx.voice_client.disconnect()
 
     @commands.command()
     async def list(self, ctx: commands.Context):
         songs = self.pg.execute(sa.select(Song)).scalars().all()
-        await ctx.send(songs)
+        description = ''
+        for i, song in enumerate(songs):
+            description += f'{i + 1}. {song.name}\n'
+        await self.send_embed(
+            ctx, description,
+            title='Song list',
+            color=discord.Colour.from_rgb(7, 133, 70)
+        )
+
+    @commands.command()
+    async def stop(self, ctx: commands.Context):
+        try:
+            await self.disconnect(ctx)
+        except AttributeError:
+            await ctx.send(self.format_message('Nothing to stop'))
 
     @commands.command()
     async def play(self, ctx: commands.Context, ident: int):
@@ -42,20 +70,32 @@ class Music(commands.Cog):
             if not status:
                 await ctx.author.voice.channel.connect()
         except AttributeError:
-            await ctx.send(self.format_message('Connect to a voice channel before playing.'))
+            await ctx.send(self.format_message(
+                'Connect to a voice channel before playing.'))
             return
 
         ffmpeg_opts = {}
 
-        def after_play(_):
-            print('Done')
+        def after_play(error):
+            if error:
+                print(error)
+            vc = get(self.bot.voice_clients, guild=ctx.guild)
+            if not vc:
+                return
+            future = run_coroutine_threadsafe(
+                self.disconnect(ctx), self.bot.loop)
+            try:
+                future.result()
+            except CancelledError:
+                print('Pizda')
 
         song = self.get_song(ident)
+        source = os.path.join('../music', song.filename) or song.url
 
         ctx.voice_client.play(
             discord.PCMVolumeTransformer(
                 discord.FFmpegPCMAudio(
-                    source=os.path.join('../music', song.filename) or song.url, **ffmpeg_opts
+                    source, **ffmpeg_opts
                 ), self._music_volume), after=after_play)
 
 
