4c4a95e68dd40407ea6c17daabd457c729c16dd7
Anton <piunov.doc@gmail.com>
2022-06-02 08:23:25 +0300

    [add]: adding and playing music from YT

--- src/cogs/music.py
+++ src/cogs/music.py
@@ -5,9 +5,13 @@ import discord
 import sqlalchemy as sa
 from discord.ext import commands
 from discord.utils import get
+from yt_dlp import YoutubeDL
+from youtube_search import YoutubeSearch
 
 from injectors.connections import acquire_session
 from models.music import Song
+
+MUSIC_PATH = '../music'
 
 
 class Music(commands.Cog):
@@ -36,13 +40,52 @@ class Music(commands.Cog):
             embed.url = url
         await ctx.send(embed=embed)
 
-    def get_song(self, ident: str | int) -> Song:
+    @classmethod
+    def get_audio_url(cls, link: str) -> tuple[str, str]:
+        ydl_opts = {'format': 'bestaudio'}
+
+        with YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(link, download=False)
+            song_format = next(
+                f for f in info['formats']
+                if f['acodec'] != 'none' and f['vcodec'] == 'none'
+            )
+        title = info['title']
+        url = song_format['url']
+        return title, url
+
+    def get_song_db(self, ident: str | int) -> Song:
         return self.pg.execute(
             sa.select(Song).where(Song.id == ident)).scalar_one_or_none()
+
+    def get_song_yt(self, arg: str) -> tuple[str, str]:
+        if arg.startswith('http'):
+            url = arg
+            title, _ = self.get_audio_url(url)
+        else:
+            res = YoutubeSearch(arg, 1)
+            video = res.videos[0]
+            title = video['title']
+            url = f'https://www.youtube.com{video["url_suffix"]}'
+        return title, url
 
     @classmethod
     async def disconnect(cls, ctx: commands.Context):
         await ctx.voice_client.disconnect()
+
+    @commands.command()
+    async def add(self, ctx: commands.Context, *args) -> Song:
+        args = ' '.join(args)
+        title, url = self.get_song_yt(args)
+        song = Song(
+            name=title,
+            url=url
+        )
+        self.pg.add(song)
+        self.pg.commit()
+        await self.send_embed(
+            ctx, f'Added {song.name} ', color=discord.Colour.blurple())
+        return song
 
     @commands.command()
     async def list(self, ctx: commands.Context):
@@ -55,6 +98,10 @@ class Music(commands.Cog):
             title='Song list',
             color=discord.Colour.from_rgb(7, 133, 70)
         )
+
+    @commands.command(hidden=True)
+    async def test(self, ctx: commands.Context):
+        self.get_song_yt('barby girl')
 
     @commands.command()
     async def stop(self, ctx: commands.Context):
@@ -74,8 +121,6 @@ class Music(commands.Cog):
                 'Connect to a voice channel before playing.'))
             return
 
-        ffmpeg_opts = {}
-
         def after_play(error):
             if error:
                 print(error)
@@ -89,8 +134,20 @@ class Music(commands.Cog):
             except CancelledError:
                 print('Pizda')
 
-        song = self.get_song(ident)
-        source = os.path.join('../music', song.filename) or song.url
+        song = self.get_song_db(ident)
+        if song.filename:
+            source = os.path.join(MUSIC_PATH, song.filename)
+            ffmpeg_opts = {}
+        elif song.url:
+            _, source = self.get_audio_url(song.url)
+            ffmpeg_opts = {
+                'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+                'options': '-vn'
+            }
+        else:
+            await self.send_embed(
+                ctx, 'Source not found', color=discord.Colour.red())
+            return
 
         ctx.voice_client.play(
             discord.PCMVolumeTransformer(
