b3ea88d1625c29eab27643a63a09c29b934d64e8
Anton <piunov.doc@gmail.com>
2022-06-04 21:25:39 +0300

    [add]: errors handler

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,6 @@
 import os
 
+import discord
 from discord.ext import commands
 
 from config import config
@@ -13,6 +14,23 @@ bot = commands.Bot(
 )
 
 
+async def send_embed(ctx: commands.Context, msg: str, *,
+                     title: str = None,
+                     color: discord.Colour = discord.Colour.blue(),
+                     url: str = None):
+    embed = discord.Embed()
+    if title:
+        embed.title = title
+    embed.description = msg
+    embed.colour = color
+    if url:
+        embed.url = url
+    if len(embed) >= 6000:
+        await ctx.send(embed=discord.Embed(
+            discription='Message overflow', colour=discord.Colour.red()))
+    await ctx.send(embed=embed)
+
+
 @bot.event
 async def on_ready():
     print('initializing db')
@@ -20,6 +38,31 @@ async def on_ready():
     if not os.path.exists('../music'):
         os.mkdir('../music')
     print('Ready!')
+
+
+@bot.event
+async def on_command_error(ctx: commands.Context, error: Exception):
+    if isinstance(error, commands.CommandNotFound):
+        await send_embed(
+            ctx, f'Command not found, use {bot.command_prefix}help.',
+            color=discord.Colour.red())
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await send_embed(ctx, f"""
+            Missing command parameter.
+            Missing parameter: **error.param**
+            Use **{ctx.command}help {ctx.command}** to get help.
+            """, color=discord.Colour.red())
+    elif isinstance(error, commands.NotOwner):
+        await ctx.send(f"Error: {error}")
+    else:
+        await send_embed(ctx,
+                         f'{ctx.author.nick}, idi nahui',
+                         color=discord.Colour.red()
+                         )
+        print(f"""
+            Command: {ctx.command}
+            Error log:
+            {error}""")
 
 
 for filename in os.listdir('./cogs'):
